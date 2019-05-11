963a459710df2efdb50d5e2cd119c2ceea33cc38
Jonathan McFadden <mcfaddja@uw.edu>
2019-05-10 22:51:53 -0700

    submission folder and zip file for Assignment 4

---
+++ Assgn4/McFadden-Assignment4/char_count.py
@@ -0,0 +1,22 @@
+# Jonathan McFadden
+
+import re
+import sys
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf()
+sc = SparkContext(conf=conf)
+
+lines = sc.textFile(sys.argv[1])
+words = lines.flatMap(lambda l: re.split(r'[^\w]+', l))
+
+pairs = words.map(lambda w: (w, 1))
+counts = pairs.reduceByKey(lambda n1, n2: n1 + n2)
+
+word_counts = pairs.reduceByKey(lambda n1, n2: n1 + n2)
+
+char_counts = word_counts.flatMap(lambda each: each[0]).filter(lambda char: char.isalpha()).map(lambda char: char).map(lambda c: (c, 1)).reduceByKey(lambda v1, v2: v1 + v2)
+
+
+char_counts.saveAsTextFile(sys.argv[2])
+sc.stop()
